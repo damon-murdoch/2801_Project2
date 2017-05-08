b73dd4f666035c59022e411969c64e0339bf660a
Damon Murdoch <damonmurdoch1@gmail.com>
2017-05-09 01:03:42 +1000

    Add files via upload

--- rectangles.py
+++ rectangles.py
@@ -1,6 +1,27 @@
 import random
-
+import math
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+class Rect:
+    def __init__(self,p1,p2,p3,p4,angle):
+        self.a = (math.fabs((p1[0] + p3[0]) / 2), math.fabs((p2[1] + p4[1])) / 2)
+        self.p1=self.rotate(self.a,p1,angle)
+        self.p2=self.rotate(self.a,p2,angle)
+        self.p3=self.rotate(self.a,p3,angle)
+        self.p4=self.rotate(self.a,p4,angle)
+
+    def rotate(self,origin,point,angle):
+        ox,oy=origin
+        px,py=point
+        qx = ox + math.cos(angle) * (ox-ox)-math.sin(angle)*(py-oy)
+        qy = oy + math.sin(angle) * (px-ox)+math.cos(angle)*(py-oy)
+        return qx, qy
+
+    def get_overlap(self, other):
+        pass
+
+wsize=640
 
 def ccw(p0, p1, p2):
     dx1 = p1[0] - p0[0]
@@ -17,87 +38,46 @@ def intersect(l1, l2):
             ccw(l2[0], l2[1], l1[0]) *
             ccw(l2[0], l2[1], l1[1]) <= 0)
 
-l1 = [[1,1], [10,10]]
-l2 = [[10, 1], [1,9]]
-print(l1, l2, intersect(l2, l1))
-plt.plot([l1[0][0], l1[1][0]], [l1[0][1], l1[1][1]])
-plt.plot([l2[0][0], l2[1][0]], [l2[0][1], l2[1][1]])
-plt.axis([0, 12, 0, 12])
-plt.show()
-
-class window(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        self.parent = parent
-
-    def print_rect(self, r1,r2):
-        self.parent.title = ("Rectangles.py")
-        self.pack(fill=BOTH, expand=1)
-
-        c = Canvas(self)
-        c.create_line(r1.x1,r1.y1,r1.x2,r1.y2)
-        c.create_line(r1.x2, r1.y2, r1.x4, r1.y4)
-        c.create_line(r1.x3, r1.y3, r1.x1, r1.y1)
-        c.create_line(r1.x4, r1.y4, r1.x3, r1.y3)
-
-        c.create_line(r2.x1, r2.y1, r2.x2, r2.y2)
-        c.create_line(r2.x2, r2.y2, r2.x4, r2.y4)
-        c.create_line(r2.x3, r2.y3, r2.x1, r2.y1)
-        c.create_line(r2.x4, r2.y4, r2.x3, r2.y3)
-
-        c.pack(fill=BOTH, expand=1)
-
-class rect:
-    def __init__(self,x1,y1,x2,y2,x3,y3,x4,y4):
-
-        self.x1 = x1
-        self.x2 = x2
-        self.x3 = x3
-        self.x4 = x4
-
-        self.y1 = y1
-        self.y2 = y2
-        self.y3 = y3
-        self.y4 = y4
-
-    def get_overlap(self, other):
-        pass
-
-wsize=640
-
-root = Tk()
-view = window(root)
-
 def genrect():
-    w=random.randint(0,256)
-    h=random.randint(0,256)
-    x=random.randint(0,480)
-    y = random.randint(0, 480)
+    w=random.randint(5,10)
+    h=random.randint(5,10)
+    x=random.randint(0,8)
+    y = random.randint(0,8)
     angle=random.randint(0,360)
+    #print(x,y,w,h)
+    # Bottom Left
+    # (X,Y)
+    p1 = [x,y]
 
     # Top Left
-    x1= x-w
-    y1= y-h
-
-    # Top Right
-    x2= x-w
-    y2= y
-
-    # Bottom Left
-    x3= x
-    y3= y-h
+    p2 = [x,y+h]
 
     # Bottom Right
-    x4= x
-    y4= y
+    p3 = [x+w,y]
 
-    r = rect(x1,y1,x2,y2,x3,y3,x4,y4)
+    # Top Right Right
+    p4 = [x+w,y+h]
+
+    r = Rect(p1,p2,p3,p4,angle)
     return r
 
-r1=genrect()
-r2=genrect()
+def checkintersect(r1,r2):
+    out=False
 
-view.print_rect(r1,r2)
 
-root.geometry(str(wsize)+"x"+str(wsize)+"+"+str(wsize)+"+"+str(wsize))
-root.mainloop()
+
+def plotrects(r1,r2):
+    #print(r1.p1,r1.p2,r1.p3,r1.p4,r1.a)
+    #print(r2.p1, r2.p2, r2.p3, r2.p4, r2.a)
+
+    plt.plot([r1.p1[0], r1.p3[0], r1.p4[0], r1.p2[0],r1.p1[0]], [r1.p1[1], r1.p3[1], r1.p4[1], r1.p2[1],r1.p1[1]])
+    plt.plot([r2.p1[0], r2.p3[0], r2.p4[0], r2.p2[0], r2.p1[0]], [r2.p1[1], r2.p3[1], r2.p4[1], r2.p2[1], r2.p1[1]])
+
+    plt.axis([0,32,0,32])
+    plt.grid()
+    plt.show()
+
+r1 = genrect()
+r2 = genrect()
+
+plotrects(r1,r2)
