3db3ab24f43d11f97fa08b98ccab2691857812ca
Damon Murdoch <damonmurdoch1@gmail.com>
2017-05-09 15:49:52 +1000

    Update Laddergram.py

--- Laddergram.py
+++ Laddergram.py
@@ -4,6 +4,7 @@ class lettergram:
 
     def __init__(self,file):
         self.stack = []
+        self.visited = []
         self.steps=0
         self.sidwaysmoves=0
         self.f = open(file,'r')
@@ -21,8 +22,7 @@ class lettergram:
 
         aas=0
         bbs=0
-        if len(a) != len(b):
-            return -1
+        #print(len(a),len(b),len(c))
         for i in range(len(a)):
             if a[i] == c[i]:
                 aas+=1
@@ -30,59 +30,67 @@ class lettergram:
             elif  b[i]== c[i]:
                 bbs+=1
                 pass
-        return (aas,bbs)
+        return aas,bbs
 
     def start(self,a,b):
 
         self.steps=0
         self.stack=[]
+        self.visited=[]
         self.sidwaysmoves = 0
         self.d = self.gen_dict(a)
-        out = self.recursionsolve(a,b,0,0)
+        out = self.recursionsolve(a,b,0)
         return out
 
-    def recursionsolve(self,a,b,repeats,found):
+    def recursionsolve(self,a,b,repeats):
 
         out = a
         self.steps+=1
         if len(a) != len(b) or a not in self.d or b not in self.d:
             return -1
-        h=0
         if a == b:
-            print(self.stack)
+            #print(self.stack)
             return 1
-        for i in range(len(a)):
-            if a[i]==b[i]:
-                h+=1
-            pass
         s = []
-        b = []
+        x = []
+
+        # Populate S
         for i in range(len(self.d)):
             sc = self.SameChars(a,b,self.d[i])
+            #print(sc)
             if sc[0] == 3:
                 if sc[1] == 1:
                     s.append(self.d[i])
                 # Not an optimal move
                 else:
-                    b.append(self.d[i])
-        print("S",s)
-        print("H",self.stack)
+                    x.append(self.d[i])
+
+        print("Options",s)
+        print("Stack",self.stack)
+        print("Visited",self.visited)
         if len(s) > repeats:
             out = s[repeats]
             self.stack.append(out)
-            self.recursionsolve(out,b,0,found+1)
+            self.recursionsolve(out,b,0)
         else:
-            self.recursionsolve(out, b, 0, found + 1)
-            '''
-            if len(self.stack) > 0:
-                self.recursionsolve(self.stack.pop(),b,repeats+1,found-1)
+            if len(x) > repeats:
+                if x[repeats] not in self.visited:
+                    out = x[repeats]
+                    self.stack.append(out)
+                    self.visited.append(out)
+                    self.recursionsolve(out,b,0)
+                else:
+                    self.recursionsolve(out,b,repeats+1)
+
+            elif len(self.stack) > 0:
+                self.recursionsolve(self.stack.pop(),b,repeats+1)
             else:
                 return -1
-            '''
+
 # Run-time Variables
 l = lettergram('dictionary.txt')
-a = input("Enter Starting word: ").lower()
-b = input("Enter finishing word:").lower()
+a = input("Enter Starting word: ").lower().rstrip()
+b = input("Enter finishing word:").lower().rstrip()
 print(a,b)
 s=l.start(a,b)
 print(s)
