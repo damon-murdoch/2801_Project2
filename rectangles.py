d71f4759098bddb0fd02a1b01e7c5fba707869ce
Damon Murdoch <damonmurdoch1@gmail.com>
2017-05-10 02:29:58 +1000

    Update rectangles.py

--- rectangles.py
+++ rectangles.py
@@ -1,127 +1,102 @@
-import random
-import math
-import matplotlib.pyplot as plt
+import sys
 
-class Rect:
-    def __init__(self,p1,p2,p3,p4):
-        self.p1=p1
-        self.p2=p2
-        self.p3=p3
-        self.p4=p4
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
 
-    def getLines(self):
-        return ((self.p1,self.p3),(self.p4,self.p2),(self.p3,self.p4),(self.p1,self.p2))
+class lettergram:
 
-    def get_overlap(self, other):
-        pass
+    def __init__(self,file):
 
-wsize=640
+        self.stack = []
+        self.visited = []
+        self.f = open(file,'r')
 
-def ccw(p0, p1, p2):
-    dx1 = p1[0] - p0[0]
-    dy1 = p1[1] - p0[1]
-    dx2 = p2[0] - p0[0]
-    dy2 = p2[1] - p0[1]
-    if dx1 * dy2 > dy1 * dx2: return 1
-    if dx1 * dy2 < dy1 * dx2: return -1
-    return 0
+    def gen_dict(self,a):
 
-def intersect(l1, l2):
-    return (ccw(l1[0], l1[1], l2[0]) *
-            ccw(l1[0], l1[1], l2[1]) <= 0 and
-            ccw(l2[0], l2[1], l1[0]) *
-            ccw(l2[0], l2[1], l1[1]) <= 0)
+        d = []
+        for line in self.f:
+            line=line.rstrip()
+            if len(line) == len(a):
+                d.append(line)
+        return d
 
-def rotate(origin,point,angle):
-    ox,oy=origin
-    px,py=point
-    qx = ox + math.cos(angle) * (ox-ox)-math.sin(angle)*(py-oy)
-    qy = oy + math.sin(angle) * (px-ox)+math.cos(angle)*(py-oy)
-    return qx, qy
+    def SameChars(self,a,b,c)
+        for i in range(len(a)):
+            if a[i] == c[i]:
+                aas+=1
+                pass
+            elif  b[i]== c[i]:
+                bbs+=1
+                pass
+        return aas,bbs
 
-def genrect():
-    w=random.randint(5,10)
-    h=random.randint(5,10)
-    x=random.randint(0,8)
-    y = random.randint(0,8)
-    angle=random.randint(0,360)
+    def start(self,a,b):
 
-    # Bottom Left
-    p1 = [x,y]
+        self.steps=0
+        self.stack=[]
+        self.visited=[]
+        self.sidwaysmoves = 0
+        self.d = self.gen_dict(a)
+        out = self.recursionsolve(a,b,0)
+        return out
 
-    # Top Left
-    p2 = [x,y+h]
+    def recursionsolve(self,a,b,repeats):
 
-    # Bottom Right
-    p3 = [x+w,y]
+        out = a
+        self.steps+=1
+        if len(a) != len(b) or a not in self.d or b not in self.d:
+            print ("Invalid inputs!")
+            return -1
+        if a == b:
+            #print(self.stack)
+            return 1
+        s = []
+        x = []
 
-    # Top Right Right
-    p4 = [x+w,y+h]
+        # Populate S
+        for i in range(len(self.d)):
+            sc = self.SameChars(a,b,self.d[i])
+            #print(sc)
+            if sc[0] == 3:
+                if sc[1] == 1:
+                    s.append(self.d[i])
+                # Not an optimal move
+                else:
+                    x.append(self.d[i])
 
-    a = (math.fabs((p1[0] + p3[0]) / 2), math.fabs((p2[1] + p4[1])) / 2)
-
-    p1 = rotate(a,p1,angle)
-    p2 = rotate(a,p2,angle)
-    p3 = rotate(a,p3,angle)
-    p4 = rotate(a,p4,angle)
-
-    r = Rect(p1,p2,p3,p4)
-    return r
-
-def checkintersect(r1,r2):
-    intersects=0
-
-    s1=r1.getLines()
-    s2=r2.getLines()
-
-    for i in range(len(s1)):
-        for j in range (len(s2)):
-            if intersect(s1[i],s2[j]) == True:
-                intersects += 1
-
-    return intersects
-
-def checkcontains(r1,r2):
-    intersects=0
-    s=r2.getLines()
-    ln = ((r1.p1), (r1.p1[0], r1.p1[0] + 1000))
-
-    for i in range(len(s)):
-        if intersect(ln,s[i])==True:
-            intersects+=1
-
-    return intersects
-
-def plotrects(r1,r2):
-    plt.plot([r1.p1[0], r1.p3[0], r1.p4[0], r1.p2[0],r1.p1[0]], [r1.p1[1], r1.p3[1], r1.p4[1], r1.p2[1],r1.p1[1]])
-    plt.plot([r2.p1[0], r2.p3[0], r2.p4[0], r2.p2[0], r2.p1[0]], [r2.p1[1], r2.p3[1], r2.p4[1], r2.p2[1], r2.p1[1]])
-
-    ctns = checkcontains(r1, r2)
-    if ctns ==1:
-        plt.title("Rectangle is contained within other rectangle. Intersects: "+str(ctns))
-
-    else:
-        ctns = checkcontains(r2, r1)
-        if ctns == 1:
-            plt.title("Rectangle is contained within other rectangle. Intersects: " + str(ctns))
-
-    if ctns != 1:
-        intr=checkintersect(r1,r2)
-        if intr > 0:
-            plt.title("Intersect Found! Intersects: "+str(intr))
+        print("Options",s)
+        print("Stack",self.stack)
+        print("Visited",self.visited)
+        if len(s) > repeats:
+            out = s[repeats]
+            self.stack.append(out)
+            self.recursionsolve(out,b,0)
         else:
-            plt.title("No intersections found.")
+            if len(x) > repeats:
+                if x[repeats] not in self.visited:
+                    out = x[repeats]
+                    self.stack.append(out)
+                    self.visited.append(out)
+                    self.recursionsolve(out,b,0)
+                else:
+                    self.recursionsolve(out,b,repeats+1)
 
-    plt.axis([0,32,0,32])
-    plt.grid()
-    plt.show()
+            elif len(self.stack) > 0:
+                self.recursionsolve(self.stack.pop(),b,repeats+1)
+            else:
+                return -1
 
-t1 = genrect()
-t2 = genrect()
-
-plotrects(t1,t2)
-
-t3=Rect((4,4),(16,4),(4,16),(16,16))
-t4=Rect((8,8),(12,8),(8,12),(12,12))
-
-plotrects(t3,t4)
+# Run-time Variables
+l = lettergram('dictionary.txt')
+a = input("Enter Starting word: ").lower().rstrip()
+b = input("Enter finishing word:").lower().rstrip()
+print(a,b)
+s=l.start(a,b)
+print(s)
+if s != -1:
+    sys.stdout.write("Conversion performed! Steps: "+str(l.steps)+"\n")
+else:
+    print("Conversion failed.")
